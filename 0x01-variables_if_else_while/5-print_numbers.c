61dbd2b0fa6f968c2f65c6c7a239a632f06509d6
reinhard <reinhard2057@gmail.com>
2022-06-03 14:25:29 -0700

    thid

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		printf("%d\n", d);
+	}
+
+	return (0);
+}
