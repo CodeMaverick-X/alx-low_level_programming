91b8ae00d45a5cab2d67b3db9128ba0e876e85d0
reinhard <reinhard2057@gmail.com>
2022-06-07 16:36:10 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
