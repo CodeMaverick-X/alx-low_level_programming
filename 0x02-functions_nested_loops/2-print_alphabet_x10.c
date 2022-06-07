3e7cc8f329b8944cec321c388451b133953100f1
reinhard <reinhard2057@gmail.com>
2022-06-07 22:16:06 +0000

    this is it

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabets 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char n;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 'a'; n <='z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}	
+}
