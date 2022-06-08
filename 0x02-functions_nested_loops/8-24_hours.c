c85d73d349772588f1c4a5210e2c5337f320c7c7
reinhard <reinhard2057@gmail.com>
2022-06-08 20:01:28 +0000

    this

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,14 @@ void jack_bauer(void)
 
 	for (h = 48; h <= 50; h++)
 	{
-		for (k = 48; k <= 51; k++)
+		for (k = 48; k <= 57; k++)
 		{
 			for (m = 48; m <= 53; m++)
 			{
 				for (n = 48; n <= 57; n++)
 				{
+					if (h >= 50 && k >= 52)
+						break;
 					_putchar(h);
 					_putchar(k);
 					_putchar(':');
