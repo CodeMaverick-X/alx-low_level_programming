912a83be7cbb64a13f89bf1c2f9933956cb561ec
reinhard <reinhard2057@gmail.com>
2022-06-08 19:33:23 +0000

    this

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Write a function that prints every minute 
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h, k, m, n;
+
+	for (h = '0'; h < '3'; h++)
+	{
+		for (k = '0'; k < '4'; k++)
+		{
+			for (m = '0'; m < '6'; m++)
+			{
+				for (n = '0'; n <= 9; n++)
+				{
+					_putchar(h);
+					_putchar(k);
+					_putchar(':');
+					_putchar(m);
+					_putchar(n);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
