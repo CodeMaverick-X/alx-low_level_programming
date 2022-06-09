c845203f6a96eb0b863e9e956524540cec9af049
reinhard <reinhard2057@gmail.com>
2022-06-09 23:21:04 +0000

    this

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@
 
 void print_most_numbers(void)
 {
-	int n;
+	char n;
 
-	for (n = 48; n <= 57; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != 50 && n != 52)
-		{
+		if (n != '2' && n != '4')
 			_putchar(n);
-		}
 	}
 	_putchar('\n');
 }
