deb362bb49f97b5a38fce02fb1b19f86bee94ddb
reinhard <reinhard2057@gmail.com>
2022-06-13 21:03:50 +0000

    this

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - function that swaps the values of two integers
+ *
+ * @a: intiger 1
+ * @b: intiger 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i = a;
+	int j = b;
+	*a = j;
+	*b = i;
+}
