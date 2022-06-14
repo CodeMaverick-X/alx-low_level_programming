8a43c2168bcfd9e271c56d6f00e25074ab1adcf5
reinhard <reinhard2057@gmail.com>
2022-06-14 22:24:45 +0000

    thuis

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,8 @@ int _putchar(int);
 void print_rev(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif
