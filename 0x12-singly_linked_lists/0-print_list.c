b4dd33847f85990aa98d23da43d238ec97592df6
reinhard <reinhard2057@gmail.com>
2022-07-14 20:53:54 +0000

    my first test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,21 @@
 #include "lists.h"
 /**
- * size_t print_list - func that prints all the elements of a list.
+ * print_list - func that prints all the elements of a list.
  * @h: pointer to the list
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
 
-	list_t *ptr = h;
-	size_t count = 0;	
+	size_t count = 0;
 
-	while (ptr->next != NULL)
+	while (h)
 	{
-		if (ptr->str)
-		{
-		printf("[%u] %s\n",ptr->len, ptr->str);
-		}
-		else
+		if (!h->str)
 			printf("[0] (nil)\n");
-		count++;
-		ptr = ptr->next;
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 	}
 	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,8 +3,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: lenght pof string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
 {
 	char str;
 	unsigned int len;
