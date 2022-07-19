06bbf4fc4cbadf893cd93c3fa618af8179b04a0f
reinhard <reinhard2057@gmail.com>
2022-07-19 10:48:40 +0000

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - function that deletes the head node
+ * @head: list head
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
