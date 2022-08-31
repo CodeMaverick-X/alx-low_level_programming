60f35b221f30f0e6fc9b509cddadc585204d620e
reinhard <reinhard2057@gmail.com>
2022-08-31 18:44:46 +0000

    this

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		tmp = tmp->next;
 		len++;
 	}
-	if (index < 0 || index > len)
+	if ((index < 0) || (index > len))
 		return (NULL);
 
 	tmp = head;
