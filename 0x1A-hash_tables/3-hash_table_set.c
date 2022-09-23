1cbee49c87275ae806d349122c98e4acb9ac7ca2
reinhardcode <reinhard2057.com>
2022-09-23 10:31:33 +0000

    this

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,33 +2,37 @@
 /**
  * hash_table_set - function that adds an element
  * to tyhe hash table
- *
  * @ht: ht is the hash table you wabìnt to add to
  * @key: the key of the element you want to add
  * @value: the value of the element you want to add
- *
  * Return: 1 if succeeded or 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
+	hash_node_t *new, *tmp;
 
 	if (!ht || !key || !strcmp(key, "") || !value)
 		return (0);
-
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = (ht->array)[index];
+	while (tmp != NULL)
+	{
+		if (!strcmp(tmp->key, key))
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-
 	if ((ht->array)[index] == NULL)
 	{
 		(ht->array)[index] = new;
@@ -40,8 +44,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		(ht->array)[index] = new;
 		return (1);
 	}
-
-
-	return (0);
-
 }
